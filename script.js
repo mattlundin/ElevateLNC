9ee60cfb7cdc2cc2bafeb4ca5aeb6fd3f8e7a7f1
mattlundin <74123405+mattlundin@users.noreply.github.com>
2022-05-03 14:40:30 -0500

    meta tags take 1

--- script.js
+++ script.js
@@ -49,6 +49,15 @@ window.addEventListener('resize', () => {
 	document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
 
+//! Tab icon???
+var link = document.querySelector("link[rel~='icon']");
+if (!link) {
+	link = document.createElement('link');
+	link.rel = 'icon';
+	document.getElementsByTagName('head')[0].appendChild(link);
+}
+link.href = 'https://fontawesome.com/icons/briefcase-medical?s=solid';
+
 //*~~~* GSAP REUSABLES;
 gsap.to('.nav a', {
 	x: 0,
