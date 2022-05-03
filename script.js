2ede8b6e80ea36c46238e9e2454a1d0b0f3144f9
mattlundin <74123405+mattlundin@users.noreply.github.com>
2022-05-03 10:43:55 -0500

    code clean up 1

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-// window.onload = function () {
-// 	window.scrollTo(0, 0);
-// };
 gsap.registerPlugin(ScrollTrigger);
 
 const nav = document.querySelector('.nav');
 const navList = document.querySelectorAll('.nav-list');
 const logo = document.querySelector('.nav-logo');
-// const logoImg = document.querySelector('.nav-logo img');
 
 window.addEventListener('scroll', sizeNav);
 
@@ -75,21 +71,6 @@ for (let i = 0; i < btns.length; i++) {
 	});
 }
 
-// const banners = document.querySelectorAll('.banner-container img');
-// for (let i = 0; i < banners.length; i++) {
-// 	let banner = banners[i];
-
-// 	gsap.from(banner, {
-// 		scrollTrigger: {
-// 			trigger: banner,
-// 		},
-// 		y: 200,
-// 		opacity: 0,
-// 		start: '20px 80%',
-// 		duration: 0.7,
-// 	});
-// }
-
 const fadeBackground = document.querySelectorAll('.fade-background');
 for (let i = 0; i < fadeBackground.length; i++) {
 	let fadeBack = fadeBackground[i];
@@ -103,17 +84,6 @@ for (let i = 0; i < fadeBackground.length; i++) {
 		duration: 0.7,
 	});
 }
-
-// gsap.from('.banner-text p', {
-// 	scrollTrigger: {
-// 		trigger: '.banner-text',
-// 		start: '20px 80%',
-// 	},
-// 	opacity: 0,
-// 	y: 200,
-// 	stagger: 0.5,
-// 	duration: 0.8,
-// });
 
 const slideRight = gsap.utils.toArray('.slide-right');
 for (let i = 0; i < slideRight.length; i++) {
@@ -178,36 +148,6 @@ for (let i = 0; i < zoomIn.length; i++) {
 	});
 }
 
-// * ===== HOME PAGE SLIDER =====
-
-// console.clear();
-// let count = 0;
-// const homeImages = document.querySelectorAll('.img-content');
-// const homeText = document.querySelectorAll('.text-container');
-
-// gsap.set(homeImages, { xPercent: -100, opacity: 1, scale: 1 });
-// gsap.set(homeImages[0], { xPercent: 0, opacity: 1, scale: 1 });
-
-// function slideIt() {
-// 	gsap.to(homeImages[count], { xPercent: 100, opacity: 0.8, scale: 0.7 });
-// 	count = count < homeImages.length - 1 ? ++count : 0;
-// 	gsap.fromTo(
-// 		homeImages[count],
-// 		{ xPercent: -100 },
-// 		{ xPercent: 0, opacity: 1, scale: 1 }
-// 	);
-// 	gsap.fromTo(
-// 		homeText[count],
-// 		{ 'clip-path': 'polygon(0 0, 0 0, 0 100%, 0 100%)' },
-// 		{
-// 			'clip-path': 'polygon(100% 0, 0 0, 0 100%, 100% 100%)',
-// 			delay: 0.5,
-// 			duration: 2,
-// 		}
-// 	);
-// 	gsap.to({}, { duration: 5.5, onComplete: slideIt });
-// }
-
 // * ===== HOME =====
 
 if (document.body.classList == 'home-page') {
@@ -240,21 +180,6 @@ if (document.body.classList == 'home-page') {
 		scale: 0.5,
 		delay: 1,
 	});
-
-	// gsap.from(
-	// 	'.clip-right',
-	// 	{
-	// 		scrollTrigger: {
-	// 			trigger: '.top-left',
-	// 			start: '20px 80%',
-	// 		},
-	// 		delay: 0.5,
-	// 		duration: 0.8,
-	// 		stagger: 0.9,
-	// 		'clip-path': 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
-	// 	},
-	// 	'-=.5'
-	// );
 }
 
 const largeMedia = window.matchMedia('(min-width: 956px)');
@@ -483,27 +408,6 @@ if (document.body.classList == 'about-page') {
 			},
 			'-=1.5'
 		);
-	// .to(
-	// 	'.row',
-	// 	{
-	// 		duration: 2.5,
-	// 	},
-	// 	'-=4'
-	// )
-	// .to(
-	// 	'.about-desc',
-	// 	{
-	// 		scrollTrigger: {
-	// 			trigger: '.about-desc',
-	// 			start: '20px 80%',
-	// 		},
-	// 		'clip-path': 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
-	// 		duration: 0.4,
-	// 		stagger: 0.6,
-	// 		ease: 'none',
-	// 	},
-	// 	'-=2.5'
-	// );
 }
 //* About and why
 if (document.body.classList == 'about-page' || 'sub-page') {
@@ -577,35 +481,6 @@ if (document.body.classList == 'sub-page') {
 		);
 }
 
-//* About, Sub-page, and Contact Page
-// if (
-// 	document.body.classList == 'services-page' ||
-// 	'about-page' ||
-// 	'sub-page' ||
-// 	'contact-page'
-// ) {
-// 	let clipper = gsap.timeline({
-// 		defaults: {
-// 			ease: 'power3.out',
-// 			opacity: 1,
-// 		},
-// 	});
-// 	clipper
-// 		.from('.header', {
-// 			x: 200,
-// 			opacity: 0,
-// 			duration: 1,
-// 		})
-// 		.to(
-// 			'.head-clip',
-// 			{
-// 				'clip-path': 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
-// 				duration: 1.2,
-// 			},
-// 			'-=.5'
-// 		);
-// }
-
 //* ===== CONTACT =====
 if (document.body.classList == 'contact-page') {
 	gsap.from('.form-container h3', {
@@ -638,160 +513,6 @@ if (document.body.classList == 'contact-page') {
 			opacity: 0,
 			stagger: 4,
 			duration: 1,
-			// 'clip-path': 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)',
 		});
 	});
-
-	// gsap.to('.info-work p', {
-	// 	scrollTrigger: {
-	// 		trigger: '.info-work p'
-	// 	}
-	// })
 }
-
-// ! ========= ERASE ==========
-// gsap.from('.hire-header', {
-// 	scrollTrigger: {
-// 		trigger: '.hire-header',
-// 		start: '20px 80%',
-// 	},
-// 	opacity: 0,
-// 	x: 150,
-// 	duration: 1.2,
-// 	scrub: true,
-// 	// ease: 'elastic',
-// });
-// const blocks = gsap.utils.toArray('.block');
-// blocks.forEach((block) => {
-// 	gsap.to(block, {
-// 		scrollTrigger: {
-// 			trigger: block,
-// 			start: '10px 90%',
-// 		},
-// 		x: 0,
-// 		opacity: 1,
-// 		duration: 1.3,
-// 		'clip-path': 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)',
-// 	});
-// });
-
-// // * LARGER SCREENS FOR .BOX ELEMENTS
-// const largeMedia = window.matchMedia('(min-width: 956px)');
-// if (largeMedia.matches) {
-// 	gsap.to('.box', {
-// 		scrollTrigger: {
-// 			trigger: '.box',
-// 			start: '10px 90%',
-// 		},
-// 		delay: 0.4,
-// 		stagger: 0.5,
-// 		duration: 0.5,
-// 		'clip-path': 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
-// 	});
-// }
-// if (largeMedia.matches) {
-// 	gsap.to('.box h4', {
-// 		scrollTrigger: {
-// 			trigger: '.box h4',
-// 			start: '10px 90%',
-// 		},
-// 		y: 0,
-// 		opacity: 1,
-// 		delay: 0.4,
-// 		stagger: 0.5,
-// 		duration: 0.5,
-// 	});
-// }
-// if (largeMedia.matches) {
-// 	gsap.to(
-// 		'.box p',
-// 		{
-// 			scrollTrigger: {
-// 				trigger: '.box p',
-// 			},
-// 			y: 0,
-// 			opacity: 1,
-// 			delay: 0.5,
-// 			stagger: 0.5,
-// 			duration: 0.5,
-// 		},
-// 		'-=.3'
-// 	);
-// }
-// // * SMALLER SCREENS FOR .BOX ELEMENTS
-// const smallMedia = window.matchMedia('(max-width: 955px)');
-
-// const boxes = gsap.utils.toArray('.box');
-// const boxHeaders = gsap.utils.toArray('.box h4');
-// const boxPs = gsap.utils.toArray('.box p');
-
-// for (let i = 0; i < boxes.length; i++) {
-// 	let box = boxes[i];
-
-// 	if (smallMedia.matches) {
-// 		gsap.to(box, {
-// 			scrollTrigger: {
-// 				trigger: box,
-// 				start: '10px 90%',
-// 			},
-// 			delay: 0.4,
-// 			// stagger: 0.5,
-// 			duration: 0.8,
-// 			'clip-path': 'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
-// 		});
-// 	}
-// }
-// for (let i = 0; i < boxHeaders.length; i++) {
-// 	let boxHeader = boxHeaders[i];
-
-// 	if (smallMedia.matches) {
-// 		gsap.to(boxHeader, {
-// 			scrollTrigger: {
-// 				trigger: boxHeader,
-// 				start: '10px 90%',
-// 			},
-// 			y: 0,
-// 			opacity: 1,
-// 			delay: 0.4,
-// 			// stagger: 0.5,
-// 			duration: 0.5,
-// 		});
-// 	}
-// }
-// for (let i = 0; i < boxPs.length; i++) {
-// 	let boxP = boxPs[i];
-
-// 	if (smallMedia.matches) {
-// 		gsap.to(boxP, {
-// 			scrollTrigger: {
-// 				trigger: boxP,
-// 			},
-// 			y: 0,
-// 			opacity: 1,
-// 			delay: 0.5,
-// 			stagger: 0.6,
-// 			duration: 0.5,
-// 		});
-// 	}
-// }
-
-// const servicesTL = gsap.timeline({
-// 	ease: 'power1.out',
-// 	scrollTrigger: {
-// 		trigger: '#services',
-// 		start: 'top center',
-// 		duration: 0.5,
-// 		toggleActions: 'play none play none',
-// 	},
-// });
-// servicesTL
-// 	.from('.services-flag-container', { opacity: 0, x: 1200 })
-// 	.from('.video-container', {
-// 		x: 1200,
-// 		rotate: 720,
-// 		width: -500,
-// 		duration: 1,
-// 	})
-// 	.from('.video-container .video', { height: 50, delay: 1.2 })
-// 	.from('.services-text', { opacity: 0, y: 500 })
-// 	.from('.services-text p', { opacity: 0, duration: 0.8 });
